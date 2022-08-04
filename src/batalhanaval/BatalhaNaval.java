16fee8d1a99696ed3914dd92f3da7e90889f77a2
Samira-Kaline <36956493+Samira-Kaline@users.noreply.github.com>
2022-08-04 11:38:58 -0300

    Update BatalhaNaval.java

--- src/batalhanaval/BatalhaNaval.java
+++ src/batalhanaval/BatalhaNaval.java
@@ -1,258 +1,65 @@
 package batalhanaval;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 
 public class BatalhaNaval {
     
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
         Exception e = new Exception();
-        Jogador jogador1;
-        Jogador jogador2; 
-        String nome1,nome2 = null;
-      
+        Jogo jogo = new Jogo();
+        Jogador jogador1 = null,jogador2 = null;
         
-        while (true){
-            System.out.println("Digite o nome do jogador 1: ");
-            nome1 = s.next();
+        
+        int op = 0;
+        
+        while(op<3){
             
-            if(e.verificarNome(nome1)){
-                break;
-            }
-            else{
-                System.out.println("Digite o nome apenas com letras: ");
-            }
-        }
-        
+            
+            jogo.printMenu();
+            op = s.nextInt();
+            
+            switch(op){
+                case 1 ->{
+                    if(jogador1!=null && jogador2!=null){
+                        System.out.println("Os dois jogadores já foram adcionados");
+                        break;
+                    }
+                    else if(jogador1==null){
+                        jogador1 = jogo.criarJogador();
+                    System.out.println("Jogador 1 criado");
+                    }
+                    else{
+                        jogador2 = jogo.criarJogador();
+                        System.out.println("Jogador 2 criado");
                     
-        Campo campojogador1 = new Campo();
-        int i = 0;
-        
-        
-        while (i<=6) {
-            String tipo,linha = null;
-            int coluna = -1;
-        
-            campojogador1.printCampo();
-            
-            while (true){
-                
-                System.out.println("Digite o tipo de navio que deseja adicionar: ");
-                tipo = s.next();
-                
-                if(e.verificarNavios(tipo)){
-                    break;  
-                }
-                System.out.println("Tipo invalido!");
-                System.out.println("Valido somente: submarino, cruzador ou porta-avioes");
-            }
-            
-            
-            while (true){
-                
-                System.out.println("Digite a posicao da linha que deseja adicionar: ");
-                System.out.println("Letra de A a H: ");
-                linha = s.next();
-                
-                if(e.verificarLinha(linha)){
-                    break;  
-                }
-                System.out.println("Intervalo invalido!");
-            }
-            
-            while (true){
-                
-                System.out.println("Digite a posicao da coluna que deseja adicionar: ");
-                System.out.println("Numero de 1 a 8: ");
-                coluna =s.nextInt();
-                
-                if(e.verificarColuna(coluna)){
-                    break;  
-                }
-                System.out.println("Intervalo invalido!");
-            }
-
-            int linhac;
-            linhac = campojogador1.converter(linha);
-
-            if(campojogador1.adicionarArma(tipo, linhac-1, coluna-1)){
-                i++;
-            }
-            
-        
-        }
-        campojogador1.printCampo();
-        
-        
-        while (true){
-            System.out.println("Digite o nome do jogador 2: ");
-            nome2 = s.next();
-            
-            if(e.verificarNome(nome2)){
-                break;
-            }
-            else{
-                System.out.println("Digite o nome apenas com letras: ");
-            }
-        }
-        
-        
-        Campo campojogador2 = new Campo();      
-        
-        i = 0;
-        while (i<=6){
-            String tipo,linha = null;
-            int coluna = -1;
-        
-            campojogador2.printCampo();
-            
-            while (true){
-                
-                System.out.println("Digite o tipo de navio que deseja adicionar: ");
-                tipo = s.next();
-                
-                if(e.verificarNavios(tipo)){
-                    break;  
-                }
-                System.out.println("Tipo invalido!");
-                System.out.println("Valido somente: submarino, cruzador ou porta-avioes");
-            }
-            
-            
-            while (true){
-                
-                System.out.println("Digite a posicao da linha que deseja adicionar: ");
-                System.out.println("Letra de A a H: ");
-                linha = s.next();
-                
-                if(e.verificarLinha(linha)){
-                    break;  
-                }
-                System.out.println("Intervalo invalido!");
-            }
-            
-            while (true){
-                
-                System.out.println("Digite a posicao da coluna que deseja adicionar: ");
-                System.out.println("Numero de 1 a 8: ");
-                coluna =s.nextInt();
-                
-                if(e.verificarColuna(coluna)){
-                    break;  
-                }
-                System.out.println("Intervalo invalido!");
-            }
-
-            int linhac = campojogador1.converter(linha);
-            if(campojogador2.adicionarArma(tipo, linhac-1, coluna-1)){
-                i++;
-            }
-            
-        }
-        campojogador2.printCampo();
-        
-        jogador1 = new  Jogador(nome1,campojogador2);
-        jogador2 = new  Jogador(nome2,campojogador1);
+                    }
                     
-                
-
-                
-        int rodadas = 1;
-        while (true){
-            if (jogador1.venceu() ){
-                
-                System.out.println("O Vencedor foi: " + jogador1.getNome());
-                break;
-                
-            }
-            else if (jogador2.venceu() ){
-                
-                System.out.println("O Vencedor foi: " + jogador2.getNome());
-                break;     
-                
-            }
-            
-            System.out.println("Rodada " + rodadas);
-            
-            // Jogada do jogador 1
-            
-            System.out.println("É a vez de " + jogador1.getNome());
-            String linha ;
-            int coluna;
-            for(i=0;i<2;i++){
-                jogador1.getMeucampo().printCampo();
-                
-                
-                while (true){
-                
-                System.out.println("Digite a posicao da linha que deseja adicionar: ");
-                System.out.println("Letra de A a H: ");
-                linha = s.next();
-                
-                if(e.verificarLinha(linha)){
-                    break;  
+                    break;
                 }
-                System.out.println("Intervalo invalido!");
-            }
-            
-            while (true){
                 
-                System.out.println("Digite a posicao da coluna que deseja adicionar: ");
-                System.out.println("Numero de 1 a 8: ");
-                coluna =s.nextInt();
-                
-                if(e.verificarColuna(coluna)){
-                    break;  
+                case 2->{
+                    if(jogador1==null || jogador2==null){
+                        System.out.println("Opss!, voce não criou jogadores, não é possivel jogar!");
+                        break;
+                    }
+                    jogo.Jogar(jogador1, jogador2);
+                    
+                    System.out.println("FIM DE JOGO!");
+                    break;
                 }
-                System.out.println("Intervalo invalido!");
+                    
             }
-                
-                int linhac = campojogador1.converter(linha);
-                jogador1.registrarTiro(linhac-1, coluna-1);
-                jogador1.getMeucampo().printCampo();
-                
-            }
-            
-            // Jogada do jogador 2
-            
-            System.out.println("É a vez de " + jogador2.getNome());
-            
-            for(i=0;i<2;i++){
-                jogador2.getMeucampo().printCampo();
-                while (true){
-                
-                System.out.println("Digite a posicao da linha que deseja adicionar: ");
-                System.out.println("Letra de A a H: ");
-                linha = s.next();
-                
-                if(e.verificarLinha(linha)){
-                    break;  
-                }
-                System.out.println("Intervalo invalido!");
-            }
-            
-            while (true){
-                
-                System.out.println("Digite a posicao da coluna que deseja adicionar: ");
-                System.out.println("Numero de 1 a 8: ");
-                coluna =s.nextInt();
-                
-                if(e.verificarColuna(coluna)){
-                    break;  
-                }
-                System.out.println("Intervalo invalido!");
-            }
-
-                int linhac = campojogador1.converter(linha);
-                jogador2.registrarTiro(linhac-1, coluna-1);
-                jogador2.getMeucampo().printCampo();
-            }
-            
-            
+        
+        
         }
+
+        
+        
     
     
-}
+    }
 }
