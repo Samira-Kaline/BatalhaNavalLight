7f0c3f3ba97bbb4fc162b57a9f443da15157184e
Samira-Kaline <36956493+Samira-Kaline@users.noreply.github.com>
2022-08-06 15:48:48 -0300

    Update Jogador.java

--- src/batalhanaval/Jogador.java
+++ src/batalhanaval/Jogador.java
@@ -51,6 +51,7 @@ public class Jogador {
             System.out.println("Acertou um navio");
             cont++;
             this.setCont(cont);
+            System.out.println("Nome: " + this.getNome()+ " Cont: " + this.cont);
         }
         else{
             campoinimigo.setPosicao(linha, coluna, 'o');
